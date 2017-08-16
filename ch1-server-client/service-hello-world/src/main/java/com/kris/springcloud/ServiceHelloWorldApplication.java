99cef88d34a02161b4651014b1bcd9f7979c676f
Kairou_Zeng <kairou_zeng@kingdee.com>
2017-08-16 19:53:14 +0800

    ch1 - simple eureka server and client demo

---
+++ ch1-server-client/service-hello-world/src/main/java/com/kris/springcloud/ServiceHelloWorldApplication.java
@@ -0,0 +1,14 @@
+package com.kris.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHelloWorldApplication.class, args);
+	}
+}
